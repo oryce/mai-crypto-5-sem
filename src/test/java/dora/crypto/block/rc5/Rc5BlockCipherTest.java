e9938c714673ee1297dba03c31ecb3d206f75555
kalane15 <kalane15@yandex.ru>
2025-11-18 12:57:23 +0300

    Update Rc5BlockCipherTest.java

--- src/test/java/dora/crypto/block/rc5/Rc5BlockCipherTest.java
+++ src/test/java/dora/crypto/block/rc5/Rc5BlockCipherTest.java
@@ -3,23 +3,56 @@ package dora.crypto.block.rc5;
 import dora.crypto.block.BlockCipher;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
 import net.jqwik.api.constraints.Size;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Rc5BlockCipherTest {
 
-    private final BlockCipher blockCipher;
+    private BlockCipher blockCipher;
 
     Rc5BlockCipherTest () {
         blockCipher = new RC5BlockCipher(new RC5Parameters(64, 10, 17));
     }
 
     @Property(tries = 100)
-    void decryptedCiphertextEqualsPlaintext(
+    void decryptedCiphertextEqualsPlaintext64(
             @ForAll @Size(value = 16) byte[] plaintext,
-            @ForAll @Size(value = 16) byte[] key
+            @ForAll @Size(min = 1, max = 255) byte[] key,
+            @ForAll @IntRange(min = 0, max = 255) int r
     ) {
+        blockCipher = new RC5BlockCipher(new RC5Parameters(64, r, key.length));
+        blockCipher.init(key);
+
+        byte[] encrypted = blockCipher.encrypt(plaintext);
+        byte[] decrypted = blockCipher.decrypt(encrypted);
+
+        assertThat(decrypted).isEqualTo(plaintext);
+    }
+
+    @Property(tries = 100)
+    void decryptedCiphertextEqualsPlaintext32(
+            @ForAll @Size(value = 8) byte[] plaintext,
+            @ForAll @Size(min = 1, max = 255) byte[] key,
+            @ForAll @IntRange(min = 0, max = 255) int r
+    ) {
+        blockCipher = new RC5BlockCipher(new RC5Parameters(32, r, key.length));
+        blockCipher.init(key);
+
+        byte[] encrypted = blockCipher.encrypt(plaintext);
+        byte[] decrypted = blockCipher.decrypt(encrypted);
+
+        assertThat(decrypted).isEqualTo(plaintext);
+    }
+
+    @Property(tries = 100)
+    void decryptedCiphertextEqualsPlaintext16(
+            @ForAll @Size(value = 4) byte[] plaintext,
+            @ForAll @Size(min = 1, max = 255) byte[] key,
+            @ForAll @IntRange(min = 0, max = 255) int r
+    ) {
+        blockCipher = new RC5BlockCipher(new RC5Parameters(16, r, key.length));
         blockCipher.init(key);
 
         byte[] encrypted = blockCipher.encrypt(plaintext);
